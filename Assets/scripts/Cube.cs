f956393e5a08d7970f8f78e561bb9958e7cea7ca
OmenDak1ngg <ishmatovmarat4@gmail.com>
2025-04-16 17:00:36 +0500

    update

--- Assets/scripts/Cube.cs
+++ Assets/scripts/Cube.cs
@@ -1,36 +1,74 @@
 using System;
+using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
 public class Cube : MonoBehaviour
 {
+    [SerializeField] private ParticleSystem _effect;
+    private float _explotionRadius = 3;
+    private float _explotionForce = 2;
+
     private int _requiredSplitChance;
-    
     private int _minSplitChance = 1;
     private int _maxSplitChance = 100;
     private int _spitChanceDecayFactor = 2;
 
+    private int _generation = 0;
+
+    public int Generation => _generation;
     public event Action Clicked;
 
     private void Start()
     {
        _requiredSplitChance = _maxSplitChance;
     }
-    public void ReduceSplitChance()
-    {
-        _requiredSplitChance /= _spitChanceDecayFactor;
-    }
 
     private void OnMouseDown()
     {
+        _generation++;
         bool isSplitted = UnityEngine.Random.Range(_minSplitChance, _maxSplitChance) <= _requiredSplitChance;
 
         if (isSplitted)
         {
             Clicked?.Invoke();
         }
-        
-        Debug.Log("DESTROYED");
+
+        Explode();
+    }
+
+    private void Explode()
+    {
+        Instantiate(_effect, transform.position, transform.rotation);
+
+        foreach(var cube in GetExplodableObjects())
+        {
+            cube.AddForce(_explotionForce * UnityEngine.Random.onUnitSphere, ForceMode.Impulse);
+        }
+
         Destroy(gameObject);
+    }
+
+    private List<Rigidbody> GetExplodableObjects()
+    {
+        Collider[] hits = Physics.OverlapSphere(transform.position, _explotionRadius);
+
+        List<Rigidbody> cubes = new();
+
+        foreach(Collider hit in hits)
+        {
+            if(hit.attachedRigidbody != null) 
+                cubes.Add(hit.attachedRigidbody);
+        }
+
+        return cubes;
+    }
+
+    public void ReduceSplitChance(int generation)
+    {
+        _generation = generation;
+        _requiredSplitChance = _maxSplitChance / (int)Math.Pow(_spitChanceDecayFactor, generation);
+        Debug.Log("Chance" + _requiredSplitChance);
     }
 }
--- Assets/scripts/CubeSpawners.cs
+++ Assets/scripts/CubeSpawners.cs
@@ -1,9 +1,7 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CubeSpawners : MonoBehaviour
 {
-    [SerializeField] private SpawnPoint[] _spawnpoints;
     [SerializeField] private Cube _cube;
     [SerializeField] private Cube _prefab;
     [SerializeField] private Material[] _materials;
@@ -12,12 +10,6 @@ public class CubeSpawners : MonoBehaviour
     private int _maxCubesCount = 6;
 
     private int _scaleReduce = 2;
-    private List<SpawnPoint> avalaibleSpawnPoints;
-
-    private void Start()
-    {
-        avalaibleSpawnPoints = new List<SpawnPoint>(_spawnpoints);
-    }
 
     private void OnEnable()
     {
@@ -33,17 +25,13 @@ public class CubeSpawners : MonoBehaviour
     {
         int countOfCubes = Random.Range(_minCubesCount, _maxCubesCount);
         int minIndex = 0;
-        SpawnPoint randomSpawnPoint;
 
         for(int i = 0; i < countOfCubes; i++)
         {
             Cube newCube = Instantiate(_prefab);
             
-            randomSpawnPoint = avalaibleSpawnPoints[Random.Range(minIndex, _spawnpoints.Length)];
-            avalaibleSpawnPoints.Remove(randomSpawnPoint);
+            newCube.ReduceSplitChance(_cube.Generation);
             
-            newCube.ReduceSplitChance();
-            newCube.transform.localPosition = randomSpawnPoint.transform.localPosition;
             newCube.transform.localScale /= _scaleReduce;
             newCube.GetComponent<Renderer>().material = _materials[Random.Range(minIndex, _materials.Length)];
         }
